99f1a92492c718902b33ef2e824007bbd87afef4
wurenand <2780049236@qq.com>
2024-12-11 18:56:28 +0800

    新增- 创建大厅，以及UI

---
+++ Source/Survival/Public/UI/HUD/LobbyHUD.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/HUD/TotalHUD.h"
+#include "LobbyHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVAL_API ALobbyHUD : public ATotalHUD
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
