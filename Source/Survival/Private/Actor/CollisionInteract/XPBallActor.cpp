74d9638cca12010aeecc60e4264dfcb5bb68eac1
wurenand <2780049236@qq.com>
2025-01-12 19:12:17 +0800

    修正- 修复了经验球会被AI触发的BUG

--- Source/Survival/Private/Actor/CollisionInteract/XPBallActor.cpp
+++ Source/Survival/Private/Actor/CollisionInteract/XPBallActor.cpp
@@ -12,6 +12,10 @@ void AXPBallActor::OnInteractHit(UPrimitiveComponent* OverlappedComponent, AActo
                                  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
                                  const FHitResult& SweepResult)
 {
+	if (!OtherActor->ActorHasTag("Player"))
+	{
+		return;
+	}
 	if (bSetTargetActor)
 	{
 		return;
